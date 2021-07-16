b35f4cc8a45c6cdfe1a3405de69dd6e088bbaf1e
jin <toto006521@gmail.com>
2021-07-16 23:57:31 +0900

    refactor function code

--- Browser101/TODOLIST/main.js
+++ Browser101/TODOLIST/main.js
@@ -4,7 +4,7 @@ const plusButton = document.querySelector(".footer-button");
 const items = document.querySelector(".items");
 const input = document.querySelector(".footer-input");
 
-const addList = () => {
+const createList = (inputValue) => {
   const itemRow = document.createElement("li");
   itemRow.setAttribute("class", "item-row");
 
@@ -13,23 +13,31 @@ const addList = () => {
 
   const itemContent = document.createElement("span");
   itemContent.setAttribute("class", "item-content");
+  itemContent.innerHTML = inputValue;
 
   const itemDelete = document.createElement("button");
   itemDelete.setAttribute("class", "item-delete");
 
   const deleteIcon = document.createElement("i");
   deleteIcon.setAttribute("class", "fas fa-trash-alt");
-
-  const inputValue = input.value;
-  items.appendChild(itemRow);
-  itemRow.appendChild(item);
-  item.appendChild(itemContent);
-  itemContent.textContent = `${inputValue}`;
-  item.appendChild(itemDelete);
-  itemDelete.appendChild(deleteIcon);
   itemDelete.addEventListener("click", () => {
     items.removeChild(itemRow);
   });
+
+  itemDelete.appendChild(deleteIcon);
+  item.appendChild(itemContent);
+  item.appendChild(itemDelete);
+  itemRow.appendChild(item);
+  return itemRow;
+};
+
+const addList = () => {
+  // 이벤트 처리할 때는 보통 onAdd 식으로 사용
+  const inputValue = input.value;
+
+  const newList = createList(inputValue);
+  items.appendChild(newList);
+
   input.value = "";
 };
 
